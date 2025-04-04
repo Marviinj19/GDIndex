8068ef5b270d8a9e0f491ece022b03ac7874875a
Marviinj19 <75039322+Marviinj19@users.noreply.github.com>
2025-04-04 00:39:15 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+window.gdconfig = {
+    "siteName": "MiniNetflix", // Nombre del sitio
+    "root": "1QreqA-o8J5oxoF5JdAaxzCgf8S561PoV", // ID de la carpeta en Google Drive
+    "client_id": "", // (Opcional) Cliente OAuth si lo deseas
+    "client_secret": "", // (Opcional) Clave secreta OAuth
+    "api_key": "", // (Opcional) API Key de Google Drive
+    "enable_cors": true, // Habilitar CORS para compartir videos
+    "playInline": true, // Permitir reproducción en línea
+    "autoplay": true, // Reproducción automática
+    "theme": "dark", // Tema oscuro estilo Netflix
+    "download": true, // Permitir descarga de archivos
+};
